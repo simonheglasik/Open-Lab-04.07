9b985d4d33b9700a5041c70a47827ef7e94ed8f8
simonheglasik <simonheglasik@gmail.com>
2019-11-07 10:06:17 +0100

    04.07

--- Open-Lab-04.07/StringTools.cs
+++ Open-Lab-04.07/StringTools.cs
@@ -6,7 +6,12 @@ namespace Open_Lab_04._07
     {
         public string Reverse(string original)
         {
-            throw new NotImplementedException();
+            string a = "";
+            for(int i=original.Length-1; i >= 0; i--)
+            {
+                a = a + original[i];
+            }
+            return a;
         }
     }
 }
